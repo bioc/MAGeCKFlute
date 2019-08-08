73044ca577c72b67e454d4c76404fd668f875f07
WubingZhang <Watson5bZhang@gmail.com>
2019-08-08 00:05:57 -0400

    1.4.3

--- R/gsGetter.R
+++ R/gsGetter.R
@@ -24,19 +24,22 @@ gsGetter <- function(gmtpath = NA, type = "All", limit = c(0, Inf), organism = '
   if(is.na(gmtpath)){
     msigdb = file.path(system.file("extdata", package = "MAGeCKFlute"),
                        paste0(organism, "_msig_entrez.gmt.gz"))
-    gmtpath = gzfile(msigdb)
-    gene2path = ReadGMT(gmtpath, limit = limit)
-    close(gmtpath)
-    names(gene2path) = c("Gene","PathwayID", "PathwayName")
-    gene2path$PathwayName = paste0(toupper(substr(gene2path$PathwayName, 0, 1)),
-                                   tolower(substr(gene2path$PathwayName, 2,
-                                                  nchar(gene2path$PathwayName))))
+    gene2path = ReadGMT(msigdb, limit = limit)
   }else{
     gene2path = ReadGMT(gmtpath, limit = limit)
   }
   type = unlist(strsplit(type, "\\+"))
+  if(any(c("c3","c6","c7") %in% type) & (organism=='hsa')){
+    msigdb = file.path(system.file("extdata", package = "MAGeCKFlute"),
+                       paste0(organism, "_msig_entrez_2.gmt.gz"))
+    gene2path = rbind(gene2path, ReadGMT(msigdb, limit = limit))
+  }
   if("c2" %in% type) type = c(type, "KEGG", "REACTOME", "BIOCARTA", "PID")
   if("c3" %in% type) type = c(type, "GOBP", "GOCC", "GOMF")
+  names(gene2path) = c("Gene","PathwayID", "PathwayName")
+  gene2path$PathwayName = paste0(toupper(substr(gene2path$PathwayName, 0, 1)),
+                                 tolower(substr(gene2path$PathwayName, 2,
+                                                nchar(gene2path$PathwayName))))
   if(!"All" %in% type){
     idx = toupper(gsub("_.*", "", gene2path$PathwayID)) %in% toupper(type)
     if(sum(idx)>0) gene2path = gene2path[idx, ]
